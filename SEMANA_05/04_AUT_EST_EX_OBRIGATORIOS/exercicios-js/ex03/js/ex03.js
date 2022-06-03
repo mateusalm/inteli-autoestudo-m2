ffcd2010d90643007631c45d8741c3869e21298a
mateusalm <mateus.almeida@sou.inteli.edu.br>
2022-06-03 16:17:55 -0300

    upadate exercício 3 semana 5

--- SEMANA_05/04_AUT_EST_EX_OBRIGATORIOS/exercicios-js/ex03/js/ex03.js
+++ SEMANA_05/04_AUT_EST_EX_OBRIGATORIOS/exercicios-js/ex03/js/ex03.js
@@ -2,12 +2,14 @@ var resultado = document.getElementById('resultado')
 
 function ordenar() {
   var lista = document.getElementById('lista').value
-  var listaOrdenada = lista.split(',').sort()
-  resultado.innerHTML = `A lista ordenada é ${listaOrdenada}.`
+  var listaOrdenadaStr = lista.split(',')
+  resultado.innerHTML = `A lista ordenada é ${listaOrdenadaStr}.`
   var valorFoco = document.getElementById('valor-foco').value
-  listaOrdenada.forEach(index => {
-    if (listaOrdenada[index] == valorFoco) {
-      resultado.innerHTML += `A posição do valor foco é ${index})}.`
+  listaOrdenadaStr.forEach(index => {
+    if (listaOrdenadaStr[index] == valorFoco) {
+      resultado.innerHTML += ` A posição do valor foco é ${
+        parseInt(index) + 1
+      }.`
     }
   })
 }
